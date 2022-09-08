0c97c3f22c0a257b6714a63bf820630f68f7aacd
salim salimzada <salimsalimzada2002@gmail.com>
2022-09-08 05:11:38 +0400

    Complete the task 3

--- task 3/index.js
+++ task 3/index.js
@@ -7,6 +7,9 @@ const arr = [
 ];
 
 function mergeArr(arr) {
+  // using object key, value for comparing array'elements exist in obj or not
+  //if it doesn't exist create new obj, add new type and push values which have same type;
+
   const obj = {};
   for (let i = 0; i < arr.length - 1; i++) {
     obj[arr[i].type] ??= { type: arr[i].type, value: [] };
@@ -16,4 +19,8 @@ function mergeArr(arr) {
 }
 
 const result = mergeArr(arr);
+// return object values
 console.log(Object.values(result));
+
+// space comlexity O(n) because of creating object
+// time complexity O(n) because of for loop
